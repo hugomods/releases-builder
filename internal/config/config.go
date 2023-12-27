ad7d13614c244295699dd9533307d8b80e26859c
razonyang <razonyang@gmail.com>
2023-12-27 14:01:06 +0800

    feat: allow configuring images, authors, categories, series and tags

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,10 +4,19 @@ type Language struct {
 	Code string `yaml:"code"`
 }
 
+type Params struct {
+	Authors    []string `yaml:"authors"`
+	Categories []string `yaml:"categories"`
+	Images     []string `yaml:"images"`
+	Series     []string `yaml:"series"`
+	Tags       []string `yaml:"tags"`
+}
+
 type Config struct {
 	ContentDir   string     `yaml:"contentDir"`
 	Languages    []Language `yaml:"languages"`
 	Repositories []string   `yaml:"repositories"`
+	Params       Params     `yaml:"params"`
 }
 
 func NewConfig() *Config {
